4c4fc322efd12fd729aa37843f72f2571fec3123
Chris Stephen <chris.stephen@circleci.com>
2023-09-01 16:09:22 -0300

    Some fixes to get pg_cron working

--- pg_cron.sh
+++ pg_cron.sh
@@ -4,19 +4,19 @@ dbname="$POSTGRES_DB"
 
 # create custom config
 customconf=/etc/postgresql/custom-postgresql.conf
-echo "shared_preload_libraries = 'pg_cron'" >> $customconf
-echo "cron.database_name = '$dbname'" >> $customconf
-chown postgres $customconf
-chgrp postgres $customconf
+echo "shared_preload_libraries = 'pg_cron'" >> ${customconf}
+echo "cron.database_name = '$dbname'" >> ${customconf}
+chown postgres ${customconf}
+chgrp postgres ${customconf}
 
 cat /etc/postgresql/custom-postgresql.conf
 
 # include custom config from main config
-conf=/var/lib/postgresql/data/postgresql.conf
-found=$(grep "include = '$customconf'" $conf)
-if [ -z "$found" ]; then
-  echo "include = '$customconf'" >> $conf
+conf="${PGDATA}/postgresql.conf"
+found=$(grep "include = '$customconf'" "${conf}")
+if [ -z "${found}" ]; then
+  echo "include = '${customconf}'" >> "${conf}"
 fi
 
-cat /var/lib/postgresql/data/postgresql.conf | grep shared
-cat /var/lib/postgresql/data/postgresql.conf | grep include
+grep "shared" "${conf}"
+grep "include" "${conf}"
